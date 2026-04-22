5089c42b018bbdda748968f8acfa4a3a790114fd
SpiceyGabs <sgsquared.2@gmail.com>
2026-04-22 23:41:17 +0200

    Files 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './pages/Home.jsx'
+import './pages/Learn.jsx'
+import './pages/MoneySnapshot.jsx'
+import './pages/StrategyTracks.jsx'
+import './pages/Simlab.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
